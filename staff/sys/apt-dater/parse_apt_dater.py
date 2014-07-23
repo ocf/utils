0d6c8c23cce1d1e99e635133e883cae6f67400a9
Timmy Zhu <tzhu@ocf.berkeley.edu>
2014-07-23 11:18:01 -0700

    Add apt-dater-announce
    Minor modifications by Chris Kuehl <ckuehl@ocf.berkeley.edu>

---
+++ staff/sys/apt-dater/parse_apt_dater.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# encoding: utf8
+
+import sys
+from collections import defaultdict
+from lxml import etree
+
+t = etree.parse(sys.stdin)
+# print etree.tostring(t)
+# print t
+
+updates = t.xpath("//pkg[@hasupdate='1']")
+# print updates
+
+collected = defaultdict(list)
+
+for u in updates:
+    collected[u.xpath("../..")[0].attrib["hostname"]].append(
+        "{name}: {old} --> {new}".format(name = u.attrib["name"], old = u.attrib["version"], new = u.attrib["data"]))
+
+for host in sorted(collected.keys()):
+    print host
+    updates = collected[host]
+    print "\t", "\n\t".join(updates)

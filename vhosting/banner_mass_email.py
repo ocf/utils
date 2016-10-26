3b776809d837531a64e28587e7b8a4b5d61aaa6f
Peter Wu <peterwu@ocf.berkeley.edu>
2016-10-26 01:17:59 -0700

    ckuehl's PR comments:
    ocflib instead of reinventing the wheel
    Whitespace fixes
    Getting rid of ./banners/ and ./logs/
    PEP8

--- vhosting/banner_mass_email.py
+++ vhosting/banner_mass_email.py
@@ -1,27 +1,25 @@
 #!/usr/bin/python3
-
 import ocflib.account.search as search
-import ocflib.account.utils as utils
 import ocflib.misc.mail as mail
 
-vhost_file = "/home/s/st/staff/vhost/vhost.conf"
-target_list = "final_missing_img"
+vhost_file = '/home/s/st/staff/vhost/vhost.conf'
+target_list = 'final_missing_img'
 
-subject = "%s - Missing OCF Banner from Website"
+subject = '%s - Missing OCF Banner from Website'
 email_body = """Dear %s:
 
-The OCF staff noticed that your group's website [1] does not have the "Hosted 
-by OCF" banner required by the virtual hosting rules [2]. All student group 
-websites are required to display the "Hosted by OCF" banner as well as the 
-student group disclaimer. Unfortunately, if you do not add both of them, we 
-will be forced to disable your website to comply with the university's 
-requirements. Please add both sometime within the next 3 weeks (by May 24) to 
-prevent disabling of your website. Thanks for your cooperation and good luck 
+The OCF staff noticed that your group's website [1] does not have the "Hosted
+by OCF" banner required by the virtual hosting rules [2]. All student group
+websites are required to display the "Hosted by OCF" banner as well as the
+student group disclaimer. Unfortunately, if you do not add both of them, we
+will be forced to disable your website to comply with the university's
+requirements. Please add both sometime within the next 3 weeks (by May 24) to
+prevent disabling of your website. Thanks for your cooperation and good luck
 on finals.
 
 Your username is "%s".
 
-If you've forgotten how to login, instructions can be found here [3]. If 
+If you've forgotten how to login, instructions can be found here [3]. If
 you've forgotten your password, you can reset it here [4].
 
 Peter Wu
@@ -38,23 +36,23 @@ contact_list = []
 
 with open(target_list) as t_l:
     for line in t_l:
-        contact_list.append(line.strip("\n"))
+        contact_list.append(line.strip('\n'))
 
 with open(vhost_file) as v_h:
     for line in v_h:
-        line = line.strip("\n")
+        line = line.strip('\n')
         # Active site
-        if (line and not line.startswith("#")):
+        if (line and not line.startswith('#')):
             # Putting site into right format
-            entries = line.split(" ")
-            user = entries[0].strip("!")
-            if (entries[1] == "-"):
+            entries = line.split(' ')
+            user = entries[0].strip('!')
+            if (entries[1] == '-'):
                 site = entries[0]
             else:
                 site = entries[1]
-            site = "http://" + site.strip("!")
-            if "." not in site:
-                site += ".berkeley.edu"
+            site = 'http://' + site.strip('!')
+            if '.' not in site:
+                site += '.berkeley.edu'
             if site in contact_list:
                 username_site_pairs.append((user, site))
 
@@ -62,7 +60,7 @@ with open(vhost_file) as v_h:
 assert len(username_site_pairs) == len(contact_list)
 
 for user, site in username_site_pairs:
-    name = search.user_attrs(user)["cn"][0]
+    name = search.user_attrs(user)['cn'][0]
     try:
         mail.send_mail_user(user, subject % user, email_body % (name, user, site))
     except Exception as e:
--- vhosting/vhosting_check.py
+++ vhosting/vhosting_check.py
@@ -1,77 +1,101 @@
 #!/usr/bin/python3
-
 import argparse
+import re
 import urllib.request as req
-import os, re, requests, sys
 
-banners_dir = "./banners/"
-logs_dir = "./logs/"
-missing_img = logs_dir + "missing_img.log"
-missing_disc = logs_dir + "missing_disc.log"
-missing_both = logs_dir + "missing_both.log"
-errors = logs_dir + "errors.log"
+import ocflib.account.utils as utils
+import requests
+
+missing_img = 'missing_img.log'
+missing_disc = 'missing_disc.log'
+missing_both = 'missing_both.log'
+errors = 'errors.log'
+banner_urls = ['https://www.ocf.berkeley.edu/hosting-logos/ocfbadge_'
+               'mini8.png',
+               'https://www.ocf.berkeley.edu/hosting-logos/ocfbadge_'
+               'mini8dark.png',
+               'https://www.ocf.berkeley.edu/hosting-logos/ocfbadge_'
+               'mini8darkglow.png',
+               'https://www.ocf.berkeley.edu/hosting-logos/ocfbadge_'
+               'platinum.png',
+               'https://www.ocf.berkeley.edu/hosting-logos/ocfbadge_'
+               'silver8.png',
+               'https://www.ocf.berkeley.edu/hosting-logos/ocfbadge_'
+               'blue8.png',
+               'https://www.ocf.berkeley.edu/~peterwu/banners/binnov-'
+               '157x46.gif',
+               'https://www.ocf.berkeley.edu/~peterwu/banners/binnov-'
+               '157x46.gif.png',
+               'https://www.ocf.berkeley.edu/~peterwu/banners/host-ocf.'
+               'png',
+               'https://www.ocf.berkeley.edu/~peterwu/banners/ocfbadge_'
+               'silver-Bkgd.png',
+               'https://www.ocf.berkeley.edu/~peterwu/banners/ocfbanner.png',
+               'https://www.ocf.berkeley.edu/~peterwu/banners/ocf.png',
+               'https://www.ocf.berkeley.edu/~peterwu/banners/ocfbadge_'
+               'mini8dark-filtered.jpg',
+               'https://www.ocf.berkeley.edu/~peterwu/banners/OX2Ok.png',
+               'https://www.ocf.berkeley.edu/~peterwu/banners/OCF-Flag.png',
+               'https://www.ocf.berkeley.edu/~peterwu/banners/lighter152x41.'
+               'gif.png']
+# To-do: Readability
+# hosting_logos_prefix = "https://www.ocf.berkeley.edu/hosting-logos/"
+# banner_urls = [hosting_logos_prefix + banner_url for banner_url
+#                in banner_urls]
 # To-do: logs -> small DB
-banners = [open(banners_dir + entry, "rb").read() for entry in
-           os.listdir("./banners")]
-disclaimer = "We are a student group acting independently of the University"\
-             " of California. We take full responsibility for our"\
-             " organization and this web site."
+banners = [requests.get(banner_url).content for banner_url in banner_urls]
+disclaimer = 'We are a student group acting independently of the University'\
+             ' of California. We take full responsibility for our'\
+             ' organization and this web site.'
 img_regex = re.compile("=\"?(\S+\.png|\S+\.gif|\S+\.jpg)")
-special_strings = ["asuc", "ocf"]
+special_strings = ['asuc', 'ocf']
+
 
 def is_special(url_string):
-    # To-do: Read from log of known exceptionsk
+    # To-do: Read from log of known exceptions
     # if url_string in exceptions:
     #     return True
-    return any([special_string in url_string for special_string in 
+    return any([special_string in url_string for special_string in
                 special_strings])
 
-def check_vhosting(vhost_file):
-    with open(vhost_file, "r") as vh, open(missing_img, "w") as m_i, \
-         open(missing_disc, "w") as m_d, open(missing_both, "w") as m_b, \
-         open(errors, "w") as error_file:
-        vhosts = []
-        for line in vh:
-            line = line.strip("\n")
-            # Active site
-            if (line and not line.startswith("#") and not is_special(line)):
-                # Putting site into right format
-                entries = line.split(" ")
-                if (entries[1] == "-"):
-                    site = entries[0]
-                else:
-                    site = entries[1]
-                site = "http://" + site.strip("!")
-                if "." not in site:
-                    site += ".berkeley.edu"
-                site += "\n"
-                try:
-                    site_opened = req.urlopen(site, timeout=10)
-                    site_html = site_opened.readall().decode("utf-8")
-                    counter = 0
-                    if disclaimer not in site_html:
-                        counter += 1
-                        m_d.writelines(site)
-                    # Check if they have any OCF banner in their images
-                    img_urls = [img_url if img_url.startswith("http") else
-                                site + "/" + img_url for img_url in
-                                img_regex.findall(site_html)]
-                    imgs = [requests.get(img_url).content for img_url in
-                            img_urls]
-                    if not any([any([banner == img for img in imgs]) for banner
-                                in banners]):
-                        m_i.writelines(site)
-						if counter == 1:
-							m_b.writelines(site)
-                except Exception as e:
-                    error_file.writelines(site)
-                    error_file.writelines(str(e) + "\n\n")
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Check if vhost banner and "
-    "disclaimer exist")
-    parser.add_argument("vhost_file", type=str, nargs="+",
-                        help="OCF vhost file location")
+def check_vhosting():
+    with open(missing_img, 'w') as m_i, open(missing_disc, 'w') as m_d,\
+            open(missing_both, 'w') as m_b, open(errors, 'w') as error_file:
+        vhosts = utils.get_vhosts()
+        vhost_urls = []
+        for vhost_url in vhosts.keys():
+            if is_special(vhost_url) or any([is_special(alias) for alias in
+                                             vhosts[vhost_url]['aliases']]):
+                continue
+            vhost_urls.append('http://' + vhost_url)
+        # For log niceness
+        vhost_urls.sort()
+        for site in vhost_urls:
+            try:
+                site_opened = req.urlopen(site, timeout=10)
+                site_html = site_opened.readall().decode('utf-8')
+                counter = False
+                if disclaimer not in site_html:
+                    counter = True
+                    m_d.writelines(site)
+                # Check if they have any OCF banner in their images
+                img_urls = [img_url if img_url.startswith('http') else
+                            site + '/' + img_url for img_url in
+                            img_regex.findall(site_html)]
+                imgs = [requests.get(img_url).content for img_url in
+                        img_urls]
+                if not any([any([banner == img for img in imgs]) for banner
+                            in banners]):
+                    m_i.writelines(site)
+                    if counter:
+                        m_b.writelines(site)
+            except Exception as e:
+                error_file.writelines(site)
+                error_file.writelines(str(e) + '\n\n')
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Check if vhost banner and '
+                                     'disclaimer exist')
     args = parser.parse_args()
-    check_vhosting(args.vhost_file[0])
-
+    check_vhosting()

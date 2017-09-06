f980455270ead46bd12d1279db8d8d2f719ce9d1
Kevin Peng <kpengboy@ocf.berkeley.edu>
2017-09-06 00:24:50 -0700

    meetings: tidy up code

--- staff/meetings/meetings.py
+++ staff/meetings/meetings.py
@@ -1,3 +1,4 @@
+import re
 from collections import defaultdict
 from datetime import datetime
 from getpass import getuser
@@ -9,7 +10,6 @@ from os import umask
 from os.path import exists
 from os.path import expanduser
 from os.path import join
-from re import compile
 from shutil import copyfile
 from string import Template
 from time import strftime
@@ -20,10 +20,25 @@ USERNAME_LEN = 16
 
 
 def get_minutes_folder():
+    """Returns the path to the folder containing minutes.
+
+    Note that bod (which is stored in public_html, as they are readable on the
+    Web) is symlinked in this folder.
+    """
     return expanduser('~staff/minutes')
 
 
 def ls(state='all'):
+    """Lists everyone with the given membership state.
+
+    Args:
+        state: the membership state. 'all' lists all attendees regardless of
+               state.
+
+    Returns:
+        A list of BoD attendees with the given membership state
+
+    """
     states = get_bod_membership()
     if state != 'all':
         states = {k: v for k, v in states.items() if v == state}
@@ -31,11 +46,13 @@ def ls(state='all'):
 
 
 def quorum():
+    """Returns the quorum for this BoD meeting."""
     bod = ls(state='bod')
     return int(ceil(2 / 3 * len(bod)))
 
 
 def get_template(choice):
+    """Returns the path to the template for the given meeting type."""
     minutes_folder = get_minutes_folder()
     if exists(join(minutes_folder, choice, 'template')):
         return join(minutes_folder, choice, 'template')
@@ -43,10 +60,15 @@ def get_template(choice):
 
 
 def get_minutes_choices():
+    """Returns the choices available for meeting type."""
     return listdir(get_minutes_folder())
 
 
 def get_semester():
+    """Returns the directory name for the current semester
+
+    For example: 2017/Fall
+    """
     now = datetime.now()
     year = now.year
     month = now.month
@@ -65,10 +87,13 @@ def get_semester():
 
 
 def get_bod_minutes_path(semester=get_semester()):
+    """Gets the path to the BoD minutes directory for the given semester."""
     return get_minutes_path('bod', semester=semester)
 
 
 def get_minutes_path(choice, semester=get_semester()):
+    """Gets the path to the minutes directory for the given type and semester.
+    """
     path = join(get_minutes_folder(), choice)
     if not exists(path):
         raise ValueError('argument must be from get_minutes_choices()')
@@ -83,10 +108,12 @@ def get_minutes_path(choice, semester=get_semester()):
 
 
 def get_minutes_file():
+    """Gets the filename for the current minutes."""
     return strftime('%Y-%m-%d')
 
 
 def get_bod_membership_file(semester=get_semester()):
+    """Gets the BoD membership file for the given semester."""
     folder = get_bod_minutes_path(semester=semester)
     path = join(folder, 'membership')
     return path
@@ -126,11 +153,20 @@ def get_minutes(folder):
         list: The sorted list of minutes files in the folder
 
     """
-    pattern = compile('^[0-9]{4}-[0-9]{2}-[0-9]{2}$')
-    return sorted([i for i in listdir(folder) if pattern.match(i)])
+    pattern = re.compile('^[0-9]{4}-[0-9]{2}-[0-9]{2}$')
+    return sorted(i for i in listdir(folder) if pattern.match(i))
 
 
-def attendance(path):
+def get_attendance(path):
+    """Returns the set of attendees for the given meeting.
+
+    Args:
+        path: path to the minutes file for the desired meeting
+
+    Returns:
+        A set containing the usernames of everyone who attended that meeting
+
+    """
     attended = set()
     with open(path) as f:
         lines = f.read().splitlines()
@@ -157,6 +193,10 @@ def get_prev_semester(semester=get_semester()):
 
 
 def new_semester():
+    """Sets up a semester's directory.
+
+    Currently copies the BoD membership file from the previous semester.
+    """
     new_file = get_bod_membership_file()
     old_file = get_bod_membership_file(semester=get_prev_semester())
     copyfile(old_file, new_file)
@@ -164,30 +204,45 @@ def new_semester():
 
 
 def minutes_setup(notes, choice):
+    # Create minutes file from template if it doesn't exist
     if not exists(notes):
         copyfile(get_template(choice), notes)
         chmod(notes, 0o644)
+
     with open(notes, 'r') as f:
         s = Template(f.read())
+
+    # Substitute values
     subs = {'username': getuser(), 'start_time': strftime('%H:%M')}
     if choice == 'bod':
         subs['quorum'] = str(quorum())
     s = s.safe_substitute(subs)
+
+    # Write out file with substituted values
     with open(notes, 'w') as f:
         f.write(s)
 
 
 def minutes_done(notes, choice):
+    # Substitute in ending values
     with open(notes, 'r') as f:
         s = Template(f.read())
     s = s.safe_substitute(end_time=strftime('%H:%M'))
+
+    # Write out file with substituted values
     with open(notes, 'w') as f:
         f.write(s)
+
     if choice == 'bod':
         update_membership()
 
 
 def update_membership():
+    """Updates the membership file for the current semester.
+
+    Uses the attendance list for the current meeting to update the membership
+    file for the current semester.
+    """
     cur_status = get_bod_membership()
     bod_members = {member for member, status in cur_status.items()
                    if status == 'bod'}
@@ -196,7 +251,7 @@ def update_membership():
     minutes_files = get_minutes(minutes_path)
     if minutes_files:
         print(join(minutes_path, minutes_files[-1]))
-        attendees_today = attendance(join(minutes_path, minutes_files[-1]))
+        attendees_today = get_attendance(join(minutes_path, minutes_files[-1]))
 
     # Start new status dict. Begin by adding all BoD members.
     new_status = {member: status for member, status in cur_status.items()
@@ -214,15 +269,15 @@ def update_membership():
     # (including the current one)
     if len(minutes_files) >= 2:
         print(join(minutes_path, minutes_files[-2]))
-        attended_last_meeting = attendance(join(minutes_path,
-                                                minutes_files[-2]))
+        attended_last_meeting = get_attendance(join(minutes_path,
+                                                    minutes_files[-2]))
     else:
         last_semester_minutes_path = get_bod_minutes_path(get_prev_semester())
         last_semester_minutes = get_minutes(last_semester_minutes_path)
         assert len(last_semester_minutes) > 1, \
             'Expecting at least one BoD meeting last semester'
-        attended_last_meeting = attendance(join(last_semester_minutes_path,
-                                                last_semester_minutes[-1]))
+        attended_last_meeting = get_attendance(join(last_semester_minutes_path,
+                                                    last_semester_minutes[-1]))
 
     print(attendees_today)
     print(attended_last_meeting)

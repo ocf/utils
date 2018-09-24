a927f66f57682499088b6f7927facf0c9dc31561
Ethan Smith <ethan@ethanhs.me>
2018-09-23 21:03:13 -0700

    Initial dh-virtualenvization

---
+++ setup.py
@@ -0,0 +1,31 @@
+import pathlib
+import shutil
+import sys
+
+from setuptools import setup
+
+try:
+    with open('.version') as f:
+        VERSION = f.readline().strip()
+except IOError:
+    VERSION = 'unknown'
+
+scripts_root = pathlib.Path(sys.prefix) / 'utils'
+scripts_root.mkdir(exist_ok=True)
+
+bin_dir = scripts_root / 'bin'
+shutil.rmtree(bin_dir, ignore_errors=True)
+
+sbin_dir = scripts_root / 'sbin'
+shutil.rmtree(sbin_dir, ignore_errors=True)
+
+shutil.copytree('./bin', bin_dir)
+shutil.copytree('./sbin', sbin_dir)
+
+setup(
+    name='approve',
+    version=VERSION,
+    url='https://www.ocf.berkeley.edu/',
+    author='Open Computing Facility',
+    author_email='help@ocf.berkeley.edu',
+)

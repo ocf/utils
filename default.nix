44a1c533daf1d12ba1677239f01ae3db7401f27b
jaysa68 <jaysagarcia@berkeley.edu>
2026-04-14 00:57:38 -0700

    add comments

--- default.nix
+++ default.nix
@@ -17,13 +17,14 @@ python312Packages.buildPythonApplication {
 
   buildInputs = [ bash wakeonlan ];
 
+  # TODO turn ocfutils into a proper export from flake.nix
   propagatedBuildInputs = with python312Packages; [
     dnspython
     ocflib
     matplotlib
     tabulate
-    mysqlclient
-    python-dateutil
+    mysqlclient #makemysql
+    python-dateutil #check
   ];
 
   meta = with lib; {
